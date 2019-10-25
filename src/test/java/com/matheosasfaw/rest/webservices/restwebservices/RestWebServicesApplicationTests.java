3661c63264bf224fb3d3dceb5fdeb743a1e26dac
Matheos Asfaw <matheos.asfaw@gmail.com>
2019-10-25 04:39:58 -0700

    initial commit

---
+++ src/test/java/com/matheosasfaw/rest/webservices/restwebservices/RestWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheosasfaw.rest.webservices.restwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
