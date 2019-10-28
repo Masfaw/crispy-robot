d03efe3982732fec6ed36c20a9829b1bf666768d
Matheos Asfaw <matheos.asfaw@gmail.com>
2019-10-28 15:46:32 -0700

    added path variable to the hello world

--- src/main/java/com/matheosasfaw/rest/webservices/restwebservices/HelloWorldController.java
+++ src/main/java/com/matheosasfaw/rest/webservices/restwebservices/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.matheosasfaw.rest.webservices.restwebservices;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //Controller
 @RestController
@@ -18,5 +15,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean (){
         return new HelloWorldBean("Hello World Bean");
+    }
+
+    @GetMapping(path = "/hello-world/path-var/{name}")
+    public HelloWorldBean helloWorldPathVariable (@PathVariable String name){
+        return new HelloWorldBean("Hello World Bean " + name);
     }
 }
