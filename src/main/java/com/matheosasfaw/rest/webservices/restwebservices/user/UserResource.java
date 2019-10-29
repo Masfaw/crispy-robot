19beea488656d3b8c43a164d3653079d4e16b1c6
Matheos Asfaw <matheos.asfaw@gmail.com>
2019-10-28 19:16:54 -0700

    Added a user URI

---
+++ src/main/java/com/matheosasfaw/rest/webservices/restwebservices/user/UserResource.java
@@ -0,0 +1,24 @@
+package com.matheosasfaw.rest.webservices.restwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    @Autowired
+    private UserDaoService userDaoService;
+
+    @GetMapping(path = "/users")
+    public List<User> retrieveAllUsers(){
+        return userDaoService.findAll();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User getUser(@PathVariable int id){
+        return userDaoService.findOne(id);
+    }
+
+}
